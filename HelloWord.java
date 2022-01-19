7b0917a12ba87954da7f13680f0757a59f822d07
suvarna218 <98008576+suvarna218@users.noreply.github.com>
2022-01-19 11:34:31 -0800

    added git and scrum words

--- HelloWord.java
+++ HelloWord.java
@@ -3,7 +3,12 @@ class HelloWord{
      System.out.println("Hello Java");  
       System.out.println("Hello spring"); 
       System.out.println("Hello Rest");  
-              System.out.println("Hello AWS");  
+      System.out.println("Hello AWS"); 
+        
+       System.out.println("Hello git");  
+        System.out.println("Hello scrum");  
+
+
 
 
 
