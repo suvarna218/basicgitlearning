00cab067b0a9402e2737abbe92435deaebf1664a
suvarna218 <98008576+suvarna218@users.noreply.github.com>
2022-01-18 22:30:12 -0800

    added spring and  rest word

--- HelloWord.java
+++ HelloWord.java
@@ -1,5 +1,9 @@
 class HelloWord{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
+      System.out.println("Hello spring"); 
+      System.out.println("Hello Rest");  
+
+
     }  
 }  
